e221212b9dd111ecc97248045e06c65ce59c9a59
Yeon-Jae Jang <yeoncomi@gmail.com>
2019-05-01 13:50:31 -0700

    commit

--- js/index.js
+++ js/index.js
@@ -7,45 +7,79 @@ function dpTime() {
   let dates = now.getDate();
   let months = now.getMonth();
   let days = now.getDay();
-  let ampm = "오후 ";
+  let ampm = "PM ";
 
   if (hours > 12)
     hours -= 12;
   else
-    ampm = "오전 ";
-
-  months++; //0~11 -> 1~12
+    ampm = "AM ";
 
   days = setDays(days);
+  months = setMonth(months)
   render(months, dates, days, ampm, hours, minutes, seconds);
 }
 
 function setDays(days) {
   if (days == 1)
-    return "월요일";
+    return "Monday";
   if (days == 2)
-    return "화요일";
+    return "Tuesday";
   if (days == 3)
-    return "수요일";
+    return "Wednesday";
   if (days == 4)
-    return "목요일";
+    return "Thursday";
   if (days == 5)
-    return "금요일";
+    return "Friday";
   if (days == 6)
-    return "토요일";
+    return "Saturday";
   if (days == 0)
-    return "일요일";
+    return "Sunday";
 }
 
+function setMonth(months){
+  if (months == 0)
+    return "January";
+  if (months == 1)
+    return "Febuary";  
+  if (months == 2)
+    return "March";
+  if (months == 3)
+    return "April";
+  if (months == 4)
+    return "May";
+  if (months == 5)
+    return "June";
+  if (months == 6)
+    return "July";
+  if (months == 7)
+    return "August";
+  if (months == 8)
+    return "September";
+  if (months == 9) 
+    return "Ocotober";
+  if (months == 10)
+    return "November";
+  if (months == 11)
+    return "December";
+}
 function zeroFill(number) {
   if (number < 10)
     return "0" + number;
   else
     return number;
 }
+function nth(d) {
+  if (d > 3 && d < 21) return 'th'; 
+  switch (d % 10) {
+    case 1:  return "st";
+    case 2:  return "nd";
+    case 3:  return "rd";
+    default: return "th";
+  }
+}
 
 function render(months, dates, days, ampm, hours, minutes, seconds) {
-  var CLOCK = document.getElementById("count").innerHTML = months + "월 " + dates + "일 " + days + " " + ampm + zeroFill(hours) + ":" + zeroFill(minutes) + ":" + zeroFill(seconds);
+  var CLOCK = document.getElementById("count").innerHTML = days+ " " + months + " " +dates+ nth(dates) + " "+ zeroFill(hours) + ":" + zeroFill(minutes) + ":" + zeroFill(seconds) + " " + ampm;
 }
 
 setInterval(() => dpTime(), 1000);
